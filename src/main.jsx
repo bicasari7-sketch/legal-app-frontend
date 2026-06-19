96a807f76dce92a141368b1835ef064fd8e73b8d
bicasari7-sketch <bi.casari7@gmail.com>
2026-06-18 22:45:55 -0300

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import LegalProcessDashboardV3 from './legal_process_v3_com_settings.jsx'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <LegalProcessDashboardV3 />
+  </React.StrictMode>,
+)
